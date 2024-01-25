29d43a41820216945979ca6778eb80a376b24d20
McOtech <otienopaul36@gmail.com>
2024-01-25 16:02:59 +0300

    build: configured prettier and eslint

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 
-describe("Something", () => {
-  it("Does something", () => {
+describe('Something', () => {
+  it('Does something', () => {
     expect(1).toBe(1);
   });
 });
